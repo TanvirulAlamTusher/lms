6580ea078531aa7aa6307e5d92e0d7346068df7f
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2024-02-14 22:48:50 +0600

    Admin password complete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -15,7 +16,7 @@ class AdminController extends Controller
     //End Method
     public function AdminLogin(Request $request)
     {
-       return view('admin.admin_login');
+        return view('admin.admin_login');
     }
     //End Method
 
@@ -30,13 +31,15 @@ class AdminController extends Controller
         return redirect('/admin/login');
     }
     //End Method
-    public function AdminProfile() {
-       $id = Auth::user()->id;
-       $profileData = User::find($id);
-       return view('admin.admin_profile_view',compact('profileData'));
-    }//End Method
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    } //End Method
 
-    public function  AdminProfileStore(Request $request) {
+    public function AdminProfileStore(Request $request)
+    {
         $id = Auth::user()->id;
         $data = User::find($id);
 
@@ -46,19 +49,50 @@ class AdminController extends Controller
         $data->phone = $request->phone;
         $data->address = $request->address;
 
-        if($request->file('photo')){
+        if ($request->file('photo')) {
             $file = $request->file('photo');
-            @unlink(public_path('upload/admin_images/'.$data->photo));
-            $filename = date('YmdHi').$file->getClientOriginalName();
-            $file->move(public_path('upload/admin_images'),$filename);
+            @unlink(public_path('upload/admin_images/' . $data->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
             $data['photo'] = $filename;
         }
         $data->save();
 
         $notifaction = array('message' => 'Profile updated Successfully',
-         'alert_type' => 'success');
+            'alert_type' => 'success');
 
         return redirect()->back()->with($notifaction);
-     }//End Method
+    } //End Method
 
+    public function AdminChangePassword()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password_view', compact('profileData'));
+
+    } //end Method
+
+    public function AdminPasswordUpdate(Request $request)
+    { //validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+
+        ]);
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            $notifaction = array('message' => 'Old password does not match',
+                'alert_type' => 'error');
+
+            return back()->with($notifaction);
+        }
+        //update the new password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password),
+        ]);
+        $notifaction = array('message' => 'Password change successfully',
+            'alert_type' => 'success');
+
+        return back()->with($notifaction);
+
+    } //end Method
 }
