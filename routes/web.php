4a4a522ec7fd1cbce8575a5c8c4fcab9281193be
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2024-05-23 12:30:16 +0600

    Add user frontend half complete

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,12 @@ use App\Http\Controllers\UserController;
 Route::get('/',[UserController::class, 'Index'])->name('index');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('frontend.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/user/profile', [UserController::class, 'UserProfile'])->name('user.profile');
+
 });
 
 require __DIR__.'/auth.php';
