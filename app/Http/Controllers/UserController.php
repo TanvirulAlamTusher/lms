40db6683d03933da271dd3996e3cec3f1b8fff31
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2024-06-11 21:01:18 +0600

    user edit complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,4 +18,29 @@ class UserController extends Controller
         $profileData = User::find($id);
         return view('frontend.dashboard.edit_profile', compact('profileData'));
     }
+
+    public function UserProfileUpdate(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/' . $data->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        $notifaction = array('message' => 'Profile updated Successfully',
+            'alert_type' => 'success');
+
+        return redirect()->back()->with($notifaction);
+    }//End method
 }
